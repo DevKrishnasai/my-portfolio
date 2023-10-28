410cab8cd2188adc358f950bf96b532cbadc2166
Krishna Sai Ambati <122152880+DevKrishnasai@users.noreply.github.com>
2023-10-28 10:41:31 +0530

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -86,7 +86,7 @@ function Dropdown({ isOpen, toggle }) {
       <NavBtn onClick={toggle}>
         <a
           className="btn PrimaryBtn"
-          href="https://linkedin.com/in/pedro-sales-muniz"
+          href="https://www.linkedin.com/in/krishnasaiambati/"
           target="_blank"
           rel="noopener noreferrer"
         >
